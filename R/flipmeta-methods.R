75541131666dda6a1b2c98f9acef5beccef6f066
Filippo Gambarota <filippo.gambarota@gmail.com>
2026-05-07 17:13:10 +0200

    adding pval using permutest.fast

--- R/flipmeta-methods.R
+++ R/flipmeta-methods.R
@@ -28,3 +28,21 @@ print.flipmeta <- function(x){
     print(x$summary_table)
 }
 
+#' @export
+print.flipmeta.rma <- function(x, ...) {
+    temp_x <- x
+    class(temp_x) <- setdiff(class(temp_x), "flipmeta.rma")
+    raw_output <- capture.output({
+        summ <- summary(temp_x, ...)
+        print(summ)
+    })
+    raw_output <- gsub("pval", "pval\u1D56", raw_output)
+    cat(raw_output, sep = "\n")
+    cat(paste0(
+        "\033[3m",
+        "\u1D56", " p values are computed using the flipmeta::permutest.fast() function!",
+        "\033[23m",
+        "\n"
+    ))
+    invisible(x)
+}
